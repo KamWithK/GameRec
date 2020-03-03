db7908669168b53e7e3fbc791b2c8f15d712ef39
Kamron Bhavnagri <kamwithk@tuta.io>
2020-03-03 13:32:44 +1100

    load Wikipedia data and save word list

--- src/Main.py
+++ src/Main.py
@@ -3,6 +3,7 @@
 
 from scrapy.crawler import CrawlerProcess
 from Spiders.Wikipedia import Spider
+from gensim.corpora import WikiCorpus, dictionary, MmCorpus
 
 # Scrape Metacritic
 process = CrawlerProcess(settings={
@@ -12,5 +13,9 @@ process = CrawlerProcess(settings={
     "LOG_LEVEL": "WARNING",
 })
 
-process.crawl(Spider)
-process.start()
+#process.crawl(Spider)
+#process.start()
+
+wiki = WikiCorpus("Data/Games.xml.bz2", lemmatize=False, article_min_tokens=200)
+wiki.dictionary.filter_extremes(no_below=20, no_above=0.1)
+wiki.dictionary.save_as_text("Data/GamesBOW.txt", False)
