19712229ac0ea51a0005b092b3bf48cbf30367d1
Kamron Bhavnagri <kamwithk@tuta.io>
2020-03-04 11:24:02 +1100

    added ability to load previouslly saved dictionaries

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os
+
 from scrapy.crawler import CrawlerProcess
 from Spiders.Wikipedia import Spider
-from gensim.corpora import WikiCorpus, dictionary, MmCorpus
+from gensim.corpora import WikiCorpus, Dictionary
 
 # Scrape Metacritic
 process = CrawlerProcess(settings={
@@ -16,6 +18,16 @@ process = CrawlerProcess(settings={
 #process.crawl(Spider)
 #process.start()
 
-wiki = WikiCorpus("Data/Games.xml.bz2", lemmatize=False, article_min_tokens=200)
-wiki.dictionary.filter_extremes(no_below=20, no_above=0.1)
-wiki.dictionary.save_as_text("Data/GamesBOW.txt", False)
+# If dictionary was already created, load it
+# Otherwise ensure that the variable is kept as None (as expected by Gensim)
+path_dictionary = "Data/GamesBOW.txt"
+dictionary = Dictionary.load_from_text(path_dictionary) if os.path.exists(path_dictionary) else None
+
+# Note that the input attribute needs to be manually set
+wiki = WikiCorpus("Data/Games.xml.bz2", lemmatize=False, article_min_tokens=200, dictionary=dictionary)
+setattr(wiki, "input", wiki.fname)
+
+if not os.path.exists(path_dictionary):
+    wiki.dictionary.filter_extremes(no_below=20, no_above=0.1)
+    dictionary = wiki.dictionary
+    dictionary.save_as_text(path_dictionary, False)
