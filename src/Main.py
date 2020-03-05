4bb2db4aed2bec538a6a75edda079eb572742494
Kamron Bhavnagri <kamwithk@tuta.io>
2020-03-05 17:12:15 +1100

    file and variable naming changes for readability and efficiency

--- src/Main.py
+++ src/Main.py
@@ -11,7 +11,7 @@ from WikiSpaCyCorpus import WikiSpacyCorpus
 # Scrape Metacritic
 process = CrawlerProcess(settings={
     "FEED_FORMAT": "json",
-    "FEED_URI": "Data/Games.json",
+    "FEED_URI": "Data/Data.json",
     "COOKIES_ENABLED": "False",
     "LOG_LEVEL": "WARNING",
 })
@@ -21,17 +21,17 @@ process = CrawlerProcess(settings={
 
 # If dictionary was already created, load it
 # Otherwise ensure that the variable is kept as None (as expected by Gensim)
-path_dictionary = "Data/GamesBOW.txt"
-dictionary = Dictionary.load_from_text(path_dictionary) if os.path.exists(path_dictionary) else None
+path_dictionary = "Data/ID2Word"
+dictionary = Dictionary.load(path_dictionary) if os.path.exists(path_dictionary) else None
 
 # Note that the input attribute needs to be manually set
-wiki = WikiSpacyCorpus("Data/Games.xml", article_min_tokens=200, dictionary=dictionary)
+wiki = WikiSpacyCorpus("Data/Wikipedia.xml", article_min_tokens=200, dictionary=dictionary)
 setattr(wiki, "input", wiki.fname)
 
 if not os.path.exists(path_dictionary):
     wiki.dictionary.filter_extremes(no_below=20, no_above=0.1)
     dictionary = wiki.dictionary
-    dictionary.save_as_text(path_dictionary, False)
+    dictionary.save(path_dictionary)
 
 # Get tokens from each page
 # Replace generator with list for accessibility
